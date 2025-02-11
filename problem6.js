d902f4b5f760806bd3f6965181164b7b5b85f0c6
Suparna Dhar <suparnad806@gmail.com>
2025-02-11 22:52:59 +0600

    Delete problem6.js

--- problem6.js
+++
@@ -1,7 +0,0 @@
-//Task: Find the Longest Word in an Array
-function findLongestWord(words) {
-    return words.reduce((longest, word) => word.length > longest.length ? word : longest, "");
-}
-
-const wordsArray = ["apple", "banana", "strawberry", "grape"];
-console.log(findLongestWord(wordsArray)); 
